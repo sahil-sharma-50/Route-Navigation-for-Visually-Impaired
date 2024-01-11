b125b34f581f4069290c6e70a53d87937e3a3592
Sahil Sharma <sahilatfau@gmail.com>
2024-01-11 18:04:06 +0100

    inference.py updated

--- inference.py
+++ inference.py
@@ -7,8 +7,10 @@ import torch.nn as nn
 import segmentation_models_pytorch as smp
 from torchvision import transforms
 from PIL import Image
+from tqdm import tqdm
 
 
+# Define Segmentation Model
 class SegmentationModel(nn.Module):
   def __init__(self):
     super(SegmentationModel, self).__init__()
@@ -23,6 +25,7 @@ class SegmentationModel(nn.Module):
   def forward(self,x):
     return self.layer(x)
 
+# Colors for image instances
 colors = [   [  0,   0,   0],
         [128, 64, 128],
         [244, 35, 232],
@@ -48,6 +51,7 @@ colors = [   [  0,   0,   0],
 label_colors = dict(zip(range(20), colors))
 
 
+# Decoding Segmentation Map
 def decode_segmap(segmentation_map):
     rgb = np.zeros((segmentation_map.shape[0], segmentation_map.shape[1], 3), dtype=np.uint8)
     for label, color in label_colors.items():
@@ -58,8 +62,10 @@ def create_binary_mask(img, rgb_value):
     mask = np.all(img == np.array(rgb_value).reshape(1, 1, 3), axis=2).astype(np.uint8) * 255
     return mask
 
+# Creating Binary Mask for Ground Truth Labels of Mapillary Dataset
 def create_ground_truth_binary_mask(labels, rgb_value):
-    for filename in os.listdir(labels):
+    print("\n****** Creating Ground Truth Binary Masks: ******")
+    for filename in tqdm(os.listdir(labels), position=0, leave=True):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             image_path = os.path.join(labels, filename)
             image = cv2.imread(image_path)
@@ -67,13 +73,12 @@ def create_ground_truth_binary_mask(labels, rgb_value):
 
             if image is not None:
                 binary_mask = create_binary_mask(image, tuple(rgb_value))
-
                 binary_mask = cv2.resize(binary_mask, (512,256), interpolation=cv2.INTER_NEAREST)
 
                 output_path = os.path.join(ground_truth_dir, filename)
                 cv2.imwrite(output_path, binary_mask)
 
-    print("Ground truth binary masks created successfully!")
+    print("Ground truth binary masks saved in:", ground_truth_dir,"\n")
 
 
 if __name__ == '__main__':
@@ -98,41 +103,39 @@ if __name__ == '__main__':
     if not os.path.exists(ground_truth_dir):
         os.makedirs(ground_truth_dir)
 
-    # Convert Mapillary Labels to Binary Images and store them in ground_truth_dir
-    # create_ground_truth_binary_mask(mapillary_labels, rgb)
+    transform = transforms.Compose([
+        transforms.Resize((256, 512)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.432, 0.433, 0.424), (0.263, 0.264, 0.278)),
+    ])
 
-    '''Testing Phase:'''
+    # Convert Mapillary Labels to Binary masks and store them in ground_truth_dir
+    create_ground_truth_binary_mask(mapillary_labels, rgb)
+
     model = SegmentationModel()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
     model.to(device)
     model.eval()
-
-
-
-
     test_image_files = [f for f in os.listdir(mapillary_images) if f.endswith('.jpg')]
 
-    transform = transforms.Compose([
-        transforms.Resize((256, 512)),
-        # transforms.RandomHorizontalFlip(p=1.0),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-
     with torch.no_grad():
-        for image_file in test_image_files:
+        print('****** Creating Segmentation masks and Binary Masks: ******')
+        for image_file in tqdm(test_image_files, position=0, leave=True):
             image_path = os.path.join(mapillary_images, image_file)
             image = Image.open(image_path).convert('RGB')
-
             image = transform(image).unsqueeze(0).to(device)
-            output = model(image.to(device))
+
+            output = model(image)
             outputx = output.detach().cpu()[0]
-            decoded_ouput = decode_segmap(torch.argmax(outputx, 0))
-            numpy_array = (decoded_ouput * 255).clip(0, 255).astype(np.uint8)
-            binary_mask = create_binary_mask(numpy_array, [244, 35, 232])
+            decoded_output = decode_segmap(torch.argmax(outputx, 0))
+
+            numpy_array = (decoded_output * 255).clip(0, 255).astype(np.uint8)
+            binary_mask = create_binary_mask(numpy_array, rgb)
+
             seg_output_image = Image.fromarray(numpy_array)
             bin_output_image = Image.fromarray(binary_mask)
+
             seg_output_image.save(os.path.join(model_output_folder, f"seg_mask_{image_file}"))
             bin_output_image.save(os.path.join(prediction_binary_dir, f"{image_file.replace('.jpg', '.png')}"), format='PNG')
 
